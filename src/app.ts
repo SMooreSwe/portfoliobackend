520d37f9b600cadd8de6e09ebbe4718dcc1d3639
SMooreSwe <stevedm88@gmail.com>
2023-09-21 14:51:24 +0200

    refactored middleware

--- src/app.ts
+++ src/app.ts
@@ -28,14 +28,9 @@ const corsOptions ={
 await server.start();
 
 const optionsHandler = ((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header(
-     "Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-   );
  if (req.method == "OPTIONS") {
    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-   return res.status(200).json({});
+   return res.status(200).send('OK');
  }
 
  next();
