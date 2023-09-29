67d77a0a21234f818e4853d4866ff61eee30efdb
SMooreSwe <stevedm88@gmail.com>
2023-09-29 10:57:03 +0200

    added cors headers

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,11 @@ await server.start();
 
 const optionsHandler = ((req, res, next) => {
  if (req.method == "OPTIONS") {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
    return res.status(200).send('OK');
  }
